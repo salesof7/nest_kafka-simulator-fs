e2f124e62196060bf27e410200f73defd87a6c7f
salesof7 <jpsaless2002@gmail.com>
2023-03-26 17:45:49 -0300

    feat: create route entity schema with @nest/mongoose

--- src/routes/entities/route.entity.ts
+++ src/routes/entities/route.entity.ts
@@ -1,1 +1,23 @@
-export class Route {}
+import { Prop, raw, Schema } from '@nestjs/mongoose';
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  startPosition: { lat: number; lng: number };
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  endPosition: { lat: number; lng: number };
+}
