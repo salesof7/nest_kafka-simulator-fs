05e015586e91edeaae21dac937d65f1f81063755
salesof7 <jpsaless2002@gmail.com>
2023-03-26 22:38:15 -0300

    fix: adjust kafka register to works with async transferences

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -8,23 +8,25 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
-        name: 'KAFKA',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: process.env.KAFKA_CLIENT_ID,
-            brokers: [process.env.KAFKA_BROKER],
+        name: 'KAFKA_SERVICE',
+        useFactory: () => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: process.env.KAFKA_CLIENT_ID,
+              brokers: [process.env.KAFKA_BROKER],
+            },
+            consumer: {
+              groupId:
+                !process.env.KAFKA_CONSUMER_GROUP_ID ||
+                process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                  ? 'my-consumer-' + Math.random()
+                  : process.env.KAFKA_CONSUMER_GROUP_ID,
+            },
           },
-          consumer: {
-            groupId:
-              !process.env.KAFKA_CONSUMER_GROUP_ID ||
-              process.env.KAFKA_CONSUMER_GROUP_ID === ''
-                ? 'my-consumer-' + Math.random()
-                : process.env.KAFKA_CONSUMER_GROUP_ID,
-          },
-        },
+        }),
       },
     ]),
   ],
