ef92444180bd7e23cd76cf9cbe514a424b4099fa
salesof7 <jpsaless2002@gmail.com>
2023-03-26 22:45:12 -0300

    feat: create a function to consume new position

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,7 +1,7 @@
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
-import { ClientKafka } from '@nestjs/microservices';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
 import { Producer } from '@nestjs/microservices/external/kafka.interface';
 import {
   Controller,
@@ -65,5 +65,20 @@ export class RoutesController implements OnModuleInit {
         },
       ],
     });
+  }
+
+  @MessagePattern('route.new-position')
+  consumeNewPosition(
+    @Payload()
+    message: {
+      value: {
+        routeId: string;
+        clientId: string;
+        position: [number, number];
+        finished: boolean;
+      };
+    },
+  ) {
+    console.log(message);
   }
 }
--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,4 +1,4 @@
-import { Get, Injectable, Param } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateRouteDto } from './dto/create-route.dto';
