4dfdb8142fccb0e434e6db2af72e86def9b8307a
salesof7 <jpsaless2002@gmail.com>
2023-03-30 11:52:45 -0300

    fix: adjust docker implementation

--- .docker/mongo/init.js
+++ .docker/mongo/init.js
@@ -1,19 +1,19 @@
 db.routes.insertMany([
   {
     _id: '1',
-    title: 'Primeiro',
+    title: 'Rota I: Epic Route ABC',
     startPosition: { lat: -15.82594, lng: -47.92923 },
     endPosition: { lat: -15.82942, lng: -47.92765 },
   },
   {
     _id: '2',
-    title: 'Segundo',
+    title: 'Rota II: Epic Route BCA',
     startPosition: { lat: -15.82449, lng: -47.92756 },
     endPosition: { lat: -15.8276, lng: -47.92621 },
   },
   {
     _id: '3',
-    title: 'Terceiro',
+    title: 'Rota III: Epic Route CAB',
     startPosition: { lat: -15.82331, lng: -47.92588 },
     endPosition: { lat: -15.82758, lng: -47.92532 },
   },
--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM node:19.8.1-alpine3.16
+FROM node:18-alpine3.16
 
-RUN apk install bash
+RUN apk add bash
 
 RUN npm install -g @nestjs/cli
 
